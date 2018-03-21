a8676220716ed272ccf6272bd2db050e5940d5fe
denis <furik1102@gmail.com>
2018-03-21 18:05:27 +0200

    add_ledsson_2

--- build/js/new.js
+++ build/js/new.js
@@ -10,4 +10,22 @@ $(function() {
     $('.hidden_open_block .close').click(function(){
         $('.hidden_open_block').slideUp();
     });
+
+    $('.button_color').click(function(){
+        $(this).next('.change_color').fadeIn();
+        return false;
+    });
+
+    $('.change_color .close').click(function(){
+        $(this).parents('.change_color').fadeOut();
+
+    });
+
+
+    $('.change_color li a').click(function(){
+        $(this).parents(".change_color").find("li").find("a").removeClass("active");
+        $(this).addClass("active");
+        return false;
+
+    });
 });
